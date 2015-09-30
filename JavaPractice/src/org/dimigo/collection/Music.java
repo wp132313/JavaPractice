029f96b37e5ba0626eaad062a45f3b7cc213f7a3
wp132313 <back8100@naver.com>
2015-09-30 12:16:09 +0900

    멜론 차트 과제 제출

---
+++ JavaPractice/src/org/dimigo/collection/Music.java
@@ -0,0 +1,45 @@
+/**
+ * 
+ */
+package org.dimigo.collection;
+
+/**
+ * <pre>
+ * org.dimigo.collection
+ * 	|_ Music
+ *
+ * 1. 개요 :
+ * 2. 작성일 : 2015. 9. 30.
+ * </pre>
+ * 
+ * @author	: 백준현
+ * @version	: 1.0
+ */
+public class Music {
+	private String title;
+	private String singer;
+	
+	public Music(String title, String singer){
+		super();
+		this.title = title;  
+		this.singer = singer;  
+	}
+	
+	public String getTitle() {  
+		 	return title;  
+		 	}  
+		   public void setTitle(String title) {  
+		 		this.title = title;  
+		 	}  
+		   public String getSinger() {  
+		 		return singer;  
+		 	}  
+		   public void setSinger(String singer) {  
+		 		this.singer = singer;  
+		 	}  
+		   public String toString() {  
+		 		return "Music [title=" + title + ", singer=" + singer + "]";  
+		 	}
+	}  
+
+
